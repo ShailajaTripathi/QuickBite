d0ff2ef17b18d5be4e8f90313787eaa9fa4aafec
ShailajaTripathi <shailaja24305@gmail.com>
2025-12-20 13:13:15 +0530

    adding mock data

--- src/utils/useRestaurantMenu.js
+++ src/utils/useRestaurantMenu.js
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react";
 import { MENU_API } from "./constant";
-import Mockdata from "../mocks/Mockdata.json";
+//import Mockdata from "../mocks/Mockdata.json";
 
 
 const useRestaurantMenu = (resId) => {
@@ -8,12 +8,12 @@ const useRestaurantMenu = (resId) => {
 
   const fetchMenu = async () => {
     
-    // const data = await fetch(`${MENU_API} ${resId } &catalog_qa=undefined&submitAction=ENTER`);
-    // const json = await data?.json();
+    const data = await fetch(`${MENU_API} ${resId } &catalog_qa=undefined&submitAction=ENTER`);
+    const json = await data?.json();
 
-    // const menuData = json?.data?.cards;
-  //  setResInfo(menuData);
-  setResInfo(Mockdata.data.cards);
+    const menuData = json?.data?.cards;
+   setResInfo(menuData);
+  //setResInfo(Mockdata.data.cards);
 
   };
   
